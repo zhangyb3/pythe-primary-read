e8218ca76137b7a96284167bf7d024b68d2b0085
zhangyb3 <cheung29@foxmail.com>
2018-01-30 10:10:51 +0800

    201801301010

--- pages/toolKit/toolKitExcerpt.js
+++ pages/toolKit/toolKitExcerpt.js
@@ -12,7 +12,9 @@ Page({
     foldIcon: [],
     foldHeight: [],
     isfold: [],
-		pageNum:1,
+		pageNum: 1,
+		isLoadding: true,
+		isLoadOver: true,
 		
   },
 
@@ -75,6 +77,66 @@ Page({
     console.log(that.data.excerptInfo)
     
   },
+
+	onReachBottom: function () {
+		var that = this;
+		var pageNum = that.data.pageNum + 1
+		that.setData({
+			isLoadding: false
+		})
+		wx.request({
+
+			url: app.globalUrl + 'personal/summary/select',
+			data: {
+				studentId: wx.getStorageSync(user.StudentID),
+				pageNum: pageNum,
+				pageSize: 10,
+			},
+			header: {
+				'content-type': 'application/json' // 默认值
+			},
+			method: 'GET',
+			success: function (res) {
+				var getAllData = that.data.gradeChineseData.concat(res.data.data)
+				console.log(res.data)
+				if (res.data.data) {
+					if (res.data.data.length == 6) {
+						console.log('成功加载数据' + res.data.data.length + '条');
+						that.setData({
+							isLoadOver: true
+						})
+					} else if (res.data.data.length > 0 && res.data.data.length < 6) {
+						console.log('成功加载数据' + res.data.data.length + '条');
+						that.setData({
+							isLoadOver: false
+						})
+					}
+					that.setData({
+						gradeChineseData: getAllData,
+						pageNum: pageNum,
+						isLoadding: true
+					});
+					console.log(that.data.pageNum);
+				} else if (null == res.data.data) {
+					console.log('没有更多数据加载了哟');
+					that.setData({
+						isLoadding: true,
+						isLoadOver: false
+					});
+				}
+
+				// console.log(that.data.gradeChineseData)
+			},
+
+			fail: function (err) {
+				wx.showToast({
+					title: '数据加载失败',
+					duration: 2000
+				})
+
+			}
+		})
+	},
 
   // 获取用户设备信息
   getDevice: function () {
--- pages/toolKit/toolKitNewWord.js
+++ pages/toolKit/toolKitNewWord.js
@@ -7,7 +7,9 @@ var user = require("../../utils/user.js");
 Page({
   data: {
     deviceHeight:0,
-		pageNum:1, 
+		pageNum: 1,
+		isLoadding: true,
+		isLoadOver: true, 
   },
 
   onLoad: function () {
@@ -34,6 +36,66 @@ Page({
 		})
 
   },
+
+	onReachBottom: function () {
+		var that = this;
+		var pageNum = that.data.pageNum + 1
+		that.setData({
+			isLoadding: false
+		})
+		wx.request({
+
+			url: app.globalUrl + 'personal/rawbook',
+			data: {
+				studentId: wx.getStorageSync(user.StudentID),
+				pageNum: pageNum,
+				pageSize: 10,
+			},
+			header: {
+				'content-type': 'application/json' // 默认值
+			},
+			method: 'GET',
+			success: function (res) {
+				var getAllData = that.data.gradeChineseData.concat(res.data.data)
+				console.log(res.data)
+				if (res.data.data) {
+					if (res.data.data.length == 6) {
+						console.log('成功加载数据' + res.data.data.length + '条');
+						that.setData({
+							isLoadOver: true
+						})
+					} else if (res.data.data.length > 0 && res.data.data.length < 6) {
+						console.log('成功加载数据' + res.data.data.length + '条');
+						that.setData({
+							isLoadOver: false
+						})
+					}
+					that.setData({
+						gradeChineseData: getAllData,
+						pageNum: pageNum,
+						isLoadding: true
+					});
+					console.log(that.data.pageNum);
+				} else if (null == res.data.data) {
+					console.log('没有更多数据加载了哟');
+					that.setData({
+						isLoadding: true,
+						isLoadOver: false
+					});
+				}
+
+				// console.log(that.data.gradeChineseData)
+			},
+
+			fail: function (err) {
+				wx.showToast({
+					title: '数据加载失败',
+					duration: 2000
+				})
+
+			}
+		})
+	},
 
   // 获取用户设备信息
   getDevice: function () {
